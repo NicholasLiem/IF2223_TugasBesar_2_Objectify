2a3d8003b2a6efb1f0384361a67a83f3360d164b
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-04-18 16:46:41 +0700

    feat: adding category manager, updating category, and adding util class

---
+++ src/main/java/com/objectify/models/items/CategoryManager.java
@@ -0,0 +1,16 @@
+package com.objectify.models.items;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryManager {
+    private static List<Category> categories = new ArrayList<Category>();
+
+    public static List<Category> getCategories() {
+        return categories;
+    }
+
+    public static void addCategory(Category category) {
+        categories.add(category);
+    }
+}
