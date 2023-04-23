5eff1dda8511d8663901f84af904605c4943b531
Bitha17 <16521076@mahasiswa.itb.ac.id>
2023-04-23 13:57:44 +0700

    feat: bill and bill manager

--- src/main/java/com/objectify/models/items/ShoppingCart.java
+++ src/main/java/com/objectify/models/items/ShoppingCart.java
@@ -1,4 +1,16 @@
 package com.objectify.models.items;
 
+import java.util.*;                                                                                                                                                                                                                                                                                                          v
+
 public class ShoppingCart {
+    private Map<Product,Integer> map;
+
+    public ShoppingCart() {
+        this.map = new HashMap<>();
+    }
+
+    public void addItem(Product product, Integer quantity){
+        Integer freq = map.get(product);
+        map.put(product, (freq == null) ? quantity : freq + quantity);
+    }
 }
