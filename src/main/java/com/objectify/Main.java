231a81a6705182a7f720e944338dc3d45dbed3f1
Sofyan Firdaus <ms.firdaus777@gmail.com>
2023-05-01 00:05:44 +0700

    feat: update main

--- src/main/java/com/objectify/Main.java
+++ src/main/java/com/objectify/Main.java
@@ -1,26 +1,8 @@
 package com.objectify;
 
-import com.objectify.datastore.XMLAdapter;
-import com.objectify.datastore.enums.XMLType;
-import com.objectify.models.entities.*;
-
-import javax.xml.bind.JAXBException;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws IOException, JAXBException {
-//        ArrayList<User> expectedUsers = new ArrayList<>();
-//        expectedUsers.add(new Member(1001, true, null, "Lala", "0812", 1000));
-//        expectedUsers.add(new VIP(1002, false, null, "Lili", "0813", 500));
-//        expectedUsers.add(new Customer(1003, true, null));
-        XMLAdapter xmlAdapter = XMLAdapter.getInstance();
-//        xmlAdapter.writeData(XMLType.USERS.getFileName(), expectedUsers);
-
-        ArrayList<User> actualUsers = xmlAdapter.readData(XMLType.USERS.getFileName(), User.class);
-        for(User u: actualUsers){
-            System.out.println(u.getClass());
-        }
+    public static void main(String[] args) {
     }
-
 }
