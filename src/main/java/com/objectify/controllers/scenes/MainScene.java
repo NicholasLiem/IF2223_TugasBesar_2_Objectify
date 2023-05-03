c86ff070da4916bee49ad1cc82c1702059fa2e52
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-05-03 21:00:30 +0700

    feat: adding dashboard pane

--- src/main/java/com/objectify/controllers/scenes/MainScene.java
+++ src/main/java/com/objectify/controllers/scenes/MainScene.java
@@ -1,6 +1,7 @@
 package com.objectify.controllers.scenes;
 
 import com.objectify.controllers.components.menubar.MenuBarManager;
+import com.objectify.controllers.pages.DashboardPane;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.TabPane;
@@ -18,5 +19,16 @@ public class MainScene extends Scene {
         BorderPane root = (BorderPane) getRoot();
         root.setCenter(tabPane);
         root.setTop(menuBar);
+
+        DashboardPane dashboardPane = new DashboardPane();
+        root.setCenter(dashboardPane);
+
+        tabPane.getSelectionModel().selectedItemProperty().addListener((obs, oldTab, newTab) -> {
+            if (newTab == null) {
+                root.setCenter(dashboardPane);
+            } else {
+                root.setCenter(tabPane);
+            }
+        });
     }
 }
