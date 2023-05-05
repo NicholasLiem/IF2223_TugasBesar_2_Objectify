6a54799e999ac29ffae153764a44aa2ef530bfa6
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-05-05 16:50:57 +0700

    feat: add BillManager

--- ObjectifyMainApp/src/main/java/com/objectify/datastore/SystemPointOfSales.java
+++ ObjectifyMainApp/src/main/java/com/objectify/datastore/SystemPointOfSales.java
@@ -1,9 +1,12 @@
 package com.objectify.datastore;
 
 import com.objectify.controllers.App;
+import com.objectify.exceptions.AppNotFoundException;
+import com.objectify.exceptions.CustomException;
 import com.objectify.models.entities.UserManager;
 import com.objectify.models.items.CategoryManager;
 import com.objectify.models.items.StorageManager;
+import com.objectify.models.transactions.BillManager;
 import com.objectify.models.transactions.TransactionManager;
 
 public class SystemPointOfSales {
@@ -16,10 +19,12 @@ public class SystemPointOfSales {
     private CategoryManager categoryManager;
     private StorageManager storageManager;
 
+    private BillManager billManager;
     private TransactionManager transactionManager;
     private App app;
 
     private SystemPointOfSales(){
+        this.billManager = new BillManager();
         this.settings = new Settings();
         this.userManager = new UserManager();
         this.categoryManager = new CategoryManager();
@@ -37,6 +42,13 @@ public class SystemPointOfSales {
 
     public void setApp(App app) {
         this.app = app;
+    }
+
+    public App getApp() throws AppNotFoundException {
+        if (this.app == null) {
+            throw new AppNotFoundException();
+        }
+        return this.app;
     }
 
     public Settings getSettings() {
--- ObjectifyMainApp/src/main/java/com/objectify/models/transactions/BillManager.java
+++ ObjectifyMainApp/src/main/java/com/objectify/models/transactions/BillManager.java
@@ -1,10 +1,18 @@
 package com.objectify.models.transactions;
 
 import com.objectify.models.entities.User;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 
+@XmlRootElement(name = "BillManager")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class BillManager {
 
+    @XmlElement(name = "Bills")
     private ArrayList<Bill> bills;
 
     public BillManager() {
