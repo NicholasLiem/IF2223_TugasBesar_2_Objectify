3b537be5a44ed7c40077fa646e9d18bededa1121
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-05-06 19:14:43 +0700

    feat: delete plugin menu

--- ObjectifyMainApp/src/main/java/com/objectify/controllers/components/menubar/MenuBarManager.java
+++ ObjectifyMainApp/src/main/java/com/objectify/controllers/components/menubar/MenuBarManager.java
@@ -2,7 +2,6 @@ package com.objectify.controllers.components.menubar;
 
 import com.objectify.controllers.components.menus.AdminMenu;
 import com.objectify.controllers.components.menus.FileMenu;
-import com.objectify.controllers.components.menus.PluginMenu;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -41,7 +40,6 @@ public class MenuBarManager {
     public void initialiseMenus(){
         FileMenu fm = new FileMenu(this.tabPane);
         AdminMenu am = new AdminMenu(this.tabPane);
-        PluginMenu pm = new PluginMenu(this.tabPane);
-        menuBar.getMenus().addAll(fm, am, pm);
+        menuBar.getMenus().addAll(fm, am);
     }
 }
