7488a1c18e8245d5bf785ff30db57629fcc1f191
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-05-08 08:57:02 +0700

    feat: add icon

--- ObjectifyMainApp/src/main/java/com/objectify/controllers/App.java
+++ ObjectifyMainApp/src/main/java/com/objectify/controllers/App.java
@@ -5,7 +5,11 @@ import com.objectify.controllers.scenes.MainScene;
 import com.objectify.datastore.SystemPointOfSales;
 import com.objectify.models.items.Category;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class App extends Application {
 
@@ -15,6 +19,10 @@ public class App extends Application {
     public void start(Stage primaryStage) {
         SystemPointOfSales.getInstance().setApp(this);
         LandingScene landingScene = new LandingScene(primaryStage, mainScene);
+
+        Path resPath = Paths.get("ObjectifyMainApp", "src","resources", "images", "bmo.png");
+        System.out.println(resPath);
+        primaryStage.getIcons().add(new Image(resPath.toUri().toString()));
 
         /* Initalizing Datastore */
         System.out.println("Initializing data with JSON datastore (default)");
